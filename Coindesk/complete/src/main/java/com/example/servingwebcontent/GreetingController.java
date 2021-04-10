5e6ae71b971625a74e8ec42acd9b1f99f8b9075f
PedroG-8 <pedrog8@ua.pt>
2021-04-10 11:51:15 +0100

    Variation alert and terminal calls every minute

--- Coindesk/complete/src/main/java/com/example/servingwebcontent/GreetingController.java
+++ Coindesk/complete/src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -1,16 +1,20 @@
 package com.example.servingwebcontent;
 
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@Component
 public class GreetingController {
 
 	@GetMapping("/bitcoin")
 	public String bitcoin(@RequestParam(name="name", required=false, defaultValue="idk") String name, Model model) {
-
+		//System.out.println("Fixed delay task - " + System.currentTimeMillis() / 1000);
 		ReadJSON js = new ReadJSON();
 		String [] str = js.getBtcInfo();
 
@@ -31,5 +35,17 @@ public class GreetingController {
 			System.out.print("ERRO");
 		}
 		return "history";
+	}
+
+	@Scheduled(fixedRate = 60000)
+	public void reportCurrentTime() {
+		ReadJSON js = new ReadJSON();
+		try{
+			System.out.println(js.getBtcInfo()[0]);
+		}
+		catch(Exception e){
+			System.out.print("ERRO");
+		}
+
 	}
 }
